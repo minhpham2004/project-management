aeba6734f3ba136dd760c55d0ed6e8378297df9a
Ngoc Minh Pham <ngocminhpham2004hn@gmail.com>
2024-11-19 23:12:13 +1100

    add cognito

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -38,9 +38,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-     
+      <AuthProvider>
         <DashboardLayout>{children}</DashboardLayout>
-  
+      </AuthProvider>
     </StoreProvider>
   );
 };
